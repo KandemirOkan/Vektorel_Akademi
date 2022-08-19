5effec3f05ee5fdc72d157385179eab36b226873
KandemirOkan <110767934+KandemirOkan@users.noreply.github.com>
2022-08-19 20:30:09 +0300

    Add files via upload

---
+++ Ado.Net/Categories.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net
+{
+        public class Categories
+        {
+
+            private int categoryIDField;
+
+            private string categoryNameField;
+
+            private string descriptionField;
+
+            private string pictureField;
+
+            /// <remarks/>
+            public int CategoryID
+            {
+                get
+                {
+                    return this.categoryIDField;
+                }
+                set
+                {
+                    this.categoryIDField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string CategoryName
+            {
+                get
+                {
+                    return this.categoryNameField;
+                }
+                set
+                {
+                    this.categoryNameField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string Description
+            {
+                get
+                {
+                    return this.descriptionField;
+                }
+                set
+                {
+                    this.descriptionField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string Picture
+            {
+                get
+                {
+                    return this.pictureField;
+                }
+                set
+                {
+                    this.pictureField = value;
+                }
+            }
+        }
+    }
---
+++ Ado.Net/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Ado.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Connectionstring = @"Server=LAPTOP-04DDO8B5;Database=NORTHWND;Integrated Security=true;";
+            SqlConnection connection = new SqlConnection(Connectionstring);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "select * from Categories";
+            command.Connection = connection;
+            SqlDataReader reader = command.ExecuteReader();
+            List<Categories> categories1 = new List<Categories>();
+            while (reader.Read())
+            {
+                Categories categories = new Categories()
+                {
+                    CategoryID = Convert.ToInt32(reader[nameof(Categories.CategoryID)]),
+                    CategoryName = Convert.ToString(reader[nameof(Categories.CategoryName)]),
+                    Description = Convert.ToString(reader[nameof(Categories.Description)]),
+                };
+                categories1.Add(categories);
+            }
+            connection.Close();
+            foreach (var item in categories1)
+            {
+                Console.WriteLine("Categoryname = {0} \t ID = {1} \t Description = {2} ", item.CategoryName, item.CategoryID, item.Description);
+            }
+            Console.ReadLine();
+        }
+    }
+}
